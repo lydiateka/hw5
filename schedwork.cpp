3c0643ee7154cedc4e53f28d29b8212c1c66aaf7
Lydia Teka <teka@usc.edu>
2025-12-09 23:09:36 -0800

    Done

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,14 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleMaker(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& shifts, 
+    size_t day, 
+    size_t curr_shift);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -31,15 +38,66 @@ bool schedule(
     const size_t maxShifts,
     DailySchedule& sched
 )
+
 {
     if(avail.size() == 0U){
         return false;
     }
     sched.clear();
     // Add your code below
-
-
-
-
+    for (size_t i = 0; i < avail.size(); i++) { 
+        vector<Worker_T> daily_sched(dailyNeed, -1); 
+        sched.push_back(daily_sched); 
+    }
+    vector<size_t> shifts(avail[0].size(), 0); 
+    return scheduleMaker(avail, dailyNeed, maxShifts, sched, shifts, 0, 0);
 }
 
+bool scheduleMaker(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& shifts, 
+    size_t day, 
+    size_t curr_shift) 
+{
+    size_t num_workers = avail[0].size();
+
+    // All days are filled 
+    if (day == avail.size()) { 
+        return true; 
+    }
+    
+    else { 
+        for (size_t worker = 0; worker < num_workers; worker++) { 
+            // Current worker has not reached the max shifts and is available that day 
+            if (shifts[worker] < maxShifts && avail[day][worker]) { 
+                //Assign worker to current shift 
+                sched[day][curr_shift] = worker; 
+                shifts[worker]++; 
+                
+                bool success;
+                // If this complete's the shift limit, move on to next day
+                if (curr_shift + 1 == dailyNeed) { 
+                    success = scheduleMaker(avail, dailyNeed, maxShifts, sched, shifts, day + 1, 0);
+                }
+                //  Otherwise, move to next shift on same day
+                else {
+                    success = scheduleMaker(avail, dailyNeed, maxShifts, sched, shifts, day, curr_shift + 1); 
+                }
+                
+                // If there is a valid schedule, stop 
+                if (success) { 
+                    return true; 
+                }
+                
+                //Backtrack and try next worker
+                shifts[worker]--; 
+                sched[day][curr_shift] = -1; 
+            }
+        } 
+    }
+    return false; 
+
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordleFind(string& curr_word, size_t index, string floating, const set<string>& dict, set<string>& words);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +21,63 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+   string curr_word = in; 
+   set<string> words; 
+   wordleFind(curr_word, 0, floating, dict, words); 
+   return words; 
 
 }
 
 // Define any helper functions here
+void wordleFind(string& curr_word, size_t index, string floating, const set<string>& dict, set<string>& words) { 
+    // If there is no more floating letters and the word is in the dictionary, add it to the set 
+    if (index == curr_word.length()) { 
+        if (floating.empty() && dict.find(curr_word) != dict.end()) {
+            words.insert(curr_word); 
+        }
+        return; 
+    }
+
+    // If the current index is not a blank, recurse to the next open index
+    if (curr_word[index] != '-') {
+        wordleFind(curr_word, index + 1, floating, dict, words);
+    }
+
+    else { 
+        // Try placing each floating letter in the current index
+        for (size_t i = 0; i < floating.size(); i++) { 
+            char letter = floating[i]; 
+            curr_word[index] = letter; 
+            
+            // Remove used floating letter
+            string temp_floating = floating; 
+            temp_floating.erase(i, 1); 
+
+            // Recurse to next open position
+            wordleFind(curr_word, index + 1, temp_floating, dict, words); 
+            curr_word[index] = '-';
+        }  
+        int blanks = 0; 
+        for (size_t i = 0; i < curr_word.size(); i++) { 
+            if (curr_word[i] == '-') { 
+                blanks++;
+            }
+        }
+
+        // If there are more blanks than floating letters, try a-z in the remaining positions
+        if (blanks > (int)floating.size()) {
+            for (char letter = 'a'; letter <= 'z'; letter++) { 
+                // Skip floating letters
+                if (floating.find(letter) == string::npos) {
+                    curr_word[index] = letter; 
+                    wordleFind(curr_word, index + 1, floating, dict, words); 
+                }
+            }
+            // Backtrack 
+            curr_word[index] = '-';
+        }
+       
+    }
+
+}
+
